6c9329c7bfd7beb71c6507198eaf5e750bf9cbc6
MURONAKA HIROAKI <muronaka@to-on.com>
2014-05-03 11:43:06 +0900

    Initial Commit

---
+++ HmuLibrary/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HmuLibrary
+//
+//  Created by MURONAKA HIROAKI on 2014/05/03.
+//  Copyright (c) 2014年 hmu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ HmuLibrary/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HmuLibrary
+//
+//  Created by MURONAKA HIROAKI on 2014/05/03.
+//  Copyright (c) 2014年 hmu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
